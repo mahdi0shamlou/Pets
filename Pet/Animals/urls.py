d388ab87703e7428106a0d3fba439f4be1773047
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-06-26 08:54:30 -0400

    create some urls for animals app like list of animals and add or delete animals of users

--- Pet/Animals/urls.py
+++ Pet/Animals/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ListAnimals
+from .views import ListAnimals, UsersAnimlas, AddAnimals, DeleteAnimals
 
 urlpatterns = [
-    path('api/list/', ListAnimals.as_view(), name='Animals-list')
+    path('api/list/', ListAnimals.as_view(), name='Animals-list'),
+    path('api/list/user/', UsersAnimlas.as_view(), name='Users_animals'),
+    path('api/add/', AddAnimals.as_view(), name='add_user_animals'),
+    path('api/delete/', DeleteAnimals.as_view(), name='delete_user_animals'),
 ]
--- Pet/Animals/views.py
+++ Pet/Animals/views.py
@@ -11,3 +11,21 @@ class ListAnimals(APIView):
     def get(self, request):
         return Response('Animals List')
 
+class UsersAnimlas(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        return Response('Animals UsersAnimlas')
+
+class AddAnimals(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        return Response('Animals AddAnimals')
+
+class DeleteAnimals(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        return Response('Animals DeleteAnimals')
+
